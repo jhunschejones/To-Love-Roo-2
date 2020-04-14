a2732c39b6ee987dc843f714d5693e9a984724fc
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-04-14 00:49:20 -0700

    Clean up migrations

--- db/migrate/20200414032439_create_notes.rb
+++ db/migrate/20200414032439_create_notes.rb
@@ -1,7 +1,7 @@
 class CreateNotes < ActiveRecord::Migration[6.0]
   def change
     create_table :notes do |t|
-      t.string :text, null: false
+      t.text :text, null: false
       t.string :creator_id, null: false
       t.string :recipient_id, null: false
 
--- db/migrate/20200414071233_change_note_text_to_text.rb
+++
@@ -1,9 +0,0 @@
-class ChangeNoteTextToText < ActiveRecord::Migration[6.0]
-  def up
-    change_column :notes, :text, :text
-  end
-
-  def down
-    change_column :notes, :text, :string
-  end
-end
