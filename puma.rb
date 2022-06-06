39657fa30d9b25585ba6e534024f922f332ffd7d
Joshua Hunsche Jones <jhunschejones@github.com>
2022-06-06 12:33:31 -0500

    Small cleanups to get Vue working again and silence an extra Puma notification

--- puma.rb
+++ puma.rb
@@ -1,6 +1,8 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 3)
 threads_count = Integer(ENV['THREAD_COUNT'] || 5)
 threads threads_count, threads_count
+# https://github.com/puma/puma/blob/master/lib/puma/dsl.rb#L515
+silence_single_worker_warning
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
